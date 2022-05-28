93836b973f94f88c311ad27b3f19e01891e72d51
Rachid Essantouh <rachid.essantouh@gmail.com>
2022-05-28 13:42:52 +0200

    Step 15 done

---
+++ src/components/Row.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import moment from "moment";
+
+const Row = ({ data }) => {
+  let checkIn = moment(data.checkInDate);
+  let checkOut = moment(data.checkOutDate);
+  const [highlighted, sethighlighted] = useState(false);
+  const highlightRow = () => {
+    sethighlighted(!highlighted);
+  };
+
+  return (
+    <tr className={highlighted ? "highlight" : ""} onClick={highlightRow}>
+      <th>{data.id}</th>
+      <th>{data.title}</th>
+      <th>{data.firstName}</th>
+      <th>{data.surname}</th>
+      <th>{data.email}</th>
+      <th>{data.roomId}</th>
+      <th>{data.checkInDate}</th>
+      <th>{data.checkOutDate}</th>
+      <th>{checkOut.diff(checkIn, "days")}</th>
+    </tr>
+  );
+};
+
+export default Row;
--- src/components/SearchResults.jsx
+++ src/components/SearchResults.jsx
@@ -1,42 +1,30 @@
-import React from "react";
-import moment from "moment";
+import React, { useState } from "react";
+import Row from "./Row";
 
 function SearchResults(prop) {
   return (
-    <table class="table">
-      <thead>
-        <tr>
-          <th scope="col">Id</th>
-          <th scope="col">Title</th>
-          <th scope="col">Firstname</th>
-          <th scope="col">Surname</th>
-          <th scope="col">Email</th>
-          <th scope="col">Room Id</th>
-          <th scope="col">Check In Date</th>
-          <th scope="col">Check Out Date</th>
-          <th scope="col">Number of Nights</th>
-        </tr>
-      </thead>
-      <tbody>
-        {prop.results.map(data => {
-          let checkIn = moment(data.checkInDate);
-          let checkOut = moment(data.checkOutDate);
-          return (
-            <tr>
-              <th>{data.id}</th>
-              <th>{data.title}</th>
-              <th>{data.firstName}</th>
-              <th>{data.surname}</th>
-              <th>{data.email}</th>
-              <th>{data.roomId}</th>
-              <th>{data.checkInDate}</th>
-              <th>{data.checkOutDate}</th>
-              <th>{checkOut.diff(checkIn, "days")}</th>
-            </tr>
-          );
-        })}
-      </tbody>
-    </table>
+    <div>
+      <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">Id</th>
+            <th scope="col">Title</th>
+            <th scope="col">Firstname</th>
+            <th scope="col">Surname</th>
+            <th scope="col">Email</th>
+            <th scope="col">Room Id</th>
+            <th scope="col">Check In Date</th>
+            <th scope="col">Check Out Date</th>
+            <th scope="col">Number of Nights</th>
+          </tr>
+        </thead>
+        <tbody>
+          {prop.results.map(data => {
+            return <Row data={data} />;
+          })}
+        </tbody>
+      </table>
+    </div>
   );
 }
 
